2f30709efef35a63a6149a2fe97b95474c721767
Chamindu Nipun <117502200+Chamindu77@users.noreply.github.com>
2024-09-27 01:57:35 +0530

    Update authActions.js

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -53,7 +53,7 @@ export const register = (name, email, password, role) => async (dispatch) => {
   const body = JSON.stringify({ name, email, password, role });
 
   try {
-    const res = await axios.post('http://localhost:5000/api/v1/auth/register', body, config);
+    const res = await axios.post('https://keen-meerkat-53eff3.netlify.app/api/v1/auth/register', body, config);
 
     dispatch({
       type: REGISTER_SUCCESS,
@@ -89,7 +89,7 @@ export const login = (email, password) => async (dispatch) => {
   const body = JSON.stringify({ email, password});
 
   try {
-    const res = await axios.post('http://localhost:5000/api/v1/auth/login', body, config);
+    const res = await axios.post('https://keen-meerkat-53eff3.netlify.app/api/v1/auth/login', body, config);
 
     
     const decoded = jwtDecode(res.data.token);
