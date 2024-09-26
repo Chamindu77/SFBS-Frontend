094aa7c7eb7255b0488b5c0a76845a40ec9c7da7
Chamindu Nipun <117502200+Chamindu77@users.noreply.github.com>
2024-09-27 02:26:08 +0530

    Update authActions.js

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -84,6 +84,7 @@ export const login = (email, password) => async (dispatch) => {
     headers: {
       'Content-Type': 'application/json',
     },
+      withCredentials: true
   };
 
   const body = JSON.stringify({ email, password});
