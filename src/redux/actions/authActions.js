69e8ba67e46544ea746425066445f213e5151aa2
Chamindu Nipun <117502200+Chamindu77@users.noreply.github.com>
2024-09-27 09:22:24 +0530

    Update authActions.js

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -90,7 +90,7 @@ export const login = (email, password) => async (dispatch) => {
   const body = JSON.stringify({ email, password});
 
   try {
-    const res = await axios.post('https://sfbs-backend.vercel.app/api/v1/auth/login', body, config);
+    const res = await axios.post('https://sfbs-backend-bxu9w3s7d-chamindu-nipuns-projects.vercel.app/api/v1/auth/login', body, config);
 
     
     const decoded = jwtDecode(res.data.token);
