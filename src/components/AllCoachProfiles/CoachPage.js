1830363f523344616a14214308b8c18de61afaaa
chamindu77 <chamindunipun99@gmail.com>
2024-10-14 14:06:37 +0530

    update

--- src/components/AllCoachProfiles/CoachPage.js
+++ src/components/AllCoachProfiles/CoachPage.js
@@ -88,7 +88,7 @@ const CoachPage = () => {
               ))
             ) : (
               <div className="text-center text-gray-700">
-                <p>No Coaches Available.</p>
+                <p>No coaches found for the selected filters.</p>
               </div>
             )}
           </div>
